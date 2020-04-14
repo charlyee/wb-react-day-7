b68f51271ee7d2c3ba5e8edbd66f56327956f27c
Warren Uhrich <hello@warren.codes>
2020-04-14 10:25:30 -0600

    Add navigation to the App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navigation from './components/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>React Day 4</h1>
+      <Navigation />
     </div>
   );
 }
