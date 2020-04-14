e000633426816abfe905d9b0b4d77af8e2c848ce
Warren Uhrich <hello@warren.codes>
2020-04-14 11:40:44 -0600

    Add interfaces to Navigation; add onClick method for lifecycle testing.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
-export default class Navigation extends React.Component {
+interface INavigationProps {
+}
+
+interface INavigationState {
+    updated: boolean
+}
+
+export default class Navigation extends React.Component<INavigationProps, INavigationState> {
     constructor ( props: string )
     {
         super( props );
+        this.state = {updated:true}
         console.log( 'Constructor ran.' );
     }
     componentDidMount ()
     {
+        // This means our component was successfully constructed and rendered.
         console.log( '"componentDidMount()" ran.' );
     }
     componentDidUpdate ()
@@ -18,6 +27,10 @@ export default class Navigation extends React.Component {
     {
         console.log( '"componentWillUnmount()" ran.' );
     }
+    testUpdate()
+    {
+        this.setState( {updated: !this.state.updated} );
+    }
     render ()
     {
         console.log( 'Render ran.' );
@@ -26,7 +39,7 @@ export default class Navigation extends React.Component {
                 <h2>Website Navigation</h2>
                 <ul>
                     <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><a href="#" onClick={this.testUpdate}>About</a></li>
                     <li><a href="#">Services</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
